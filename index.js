2ad945aad7277b7b93b7651ffee3d1611defcdca
Luca Saba <lucasaba@gmail.com>
2017-11-30 18:06:20 +0100

    Adds the cors headers
    This is needed if the rapsberry pi thermostat is on
    a different machine then the frontend thermostat

--- index.js
+++ index.js
@@ -53,6 +53,12 @@ port.on('error', function(err) {
     process.exit(1);
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/exec/:command', function(req, res){
     var quit = false;
     res.header('Content-Type', 'application/json');
